994f754b934a9bf2ef7cc7d5f153f7ea1bec6419
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-02-10 23:31:38 +0100

    Updated 'seeds.rb' until it works + Added a 'empty_tables.sql' SQL script file to delete all contents of all tables (without destroying their structure though) to help...

---
+++ db/empty_tables.sql
@@ -0,0 +1,10 @@
+-- DELETE all data from each of the tables below WITHOUT DESTROYING their structure, though
+delete from appointments;
+delete from diplomas;
+delete from doctor_diplomas;
+delete from specialties;
+delete from cities;
+delete from doctors;
+delete from diploma_specialties;
+delete from patients;
+-- END of script
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+puts "*** Started at #{Time.now} ***"
+
 # DESTROY SECTION - Uncomment to truncate all SQLite tables + anihilate all created objects + empty tmp tables
+puts "[DESTROY SECTION - BEGIN]"
+puts "  > Destroying objects and deleting from tables"
 City.destroy_all
 Specialty.destroy_all
 Appointment.destroy_all
@@ -10,6 +14,7 @@ DiplomaSpecialty.destroy_all
 Diploma.destroy_all
 Doctor.destroy_all
 Patient.destroy_all
+puts "  > Initializing arrays as []"
 my_cities = []
 my_specialties = []
 my_appointments = []
@@ -18,15 +23,20 @@ my_diploma_specialties = []
 my_diplomas = []
 my_doctors = []
 my_patients = []
+puts "[DESTROY SECTION - END]"
+puts
 
 # POPULATE SECTION - Notice: order of population may be structuring (cannot load data into 'appointments' before 'doctors' and 'patients' exist ;-)
 # 01 - Populate 'cities'
+puts "[POPULATE SECTION - BEGIN]"
+puts "  > Populating 'my_cities'"
 100.times do
 	my_cities << City.create(city_name: Faker::Address.city, 
                            zip_code: Faker::Address.zip)
 end
 
 # 02 - Populate 'specialties'
+puts "  > Populating 'my_specialties'"
 10.times do |n|
   my_specialties << Specialty.create(title: ["Specialist in Allergy and Immunology",
                                              "Radiologist",
@@ -41,12 +51,14 @@ end
 end
 
 # 03 - Populate 'diplomas'
+puts "  > Populating 'my_diplomas'"
 10.times do |n|
   my_diplomas << Diploma.create(title: Faker::Job.education_level)
 end
 
 # 04 - Populate 'diploma_specialties' 
 #      (i.e for each 'diploma', add it 1 to 3 specialties taken from the 'my_specialties' array)
+puts "  > Populating 'my_diploma_specialties'"
 my_diplomas.length.times do |n|
   rand(1..3).times do |p|
     my_diploma_specialties << DiplomaSpecialty.create(diploma_id: my_diplomas[n-1].id, 
@@ -55,6 +67,7 @@ my_diplomas.length.times do |n|
 end
 
 # 05 - Populate 'patients'
+puts "  > Populating 'my_patients'"
 100.times do |n|
 	my_patients << Patient.create(first_name: Faker::Name.first_name, 
                                 last_name: Faker::Name.last_name, 
@@ -62,6 +75,7 @@ end
 end
 
 # 06 - Populate 'doctors'
+puts "  > Populating 'my_doctors'"
 20.times do
   my_doctors << Doctor.create(first_name: Faker::Name.first_name, 
                               last_name: Faker::Name.last_name, 
@@ -69,6 +83,7 @@ end
 end
 
 # 07 - Populate 'appointments'
+puts "  > Populating 'my_appointments'"
 60.times do
   my_appointments << Appointment.create(date: Faker::Time.forward(days: 365, period: :all), 
                                         doctor_id: my_doctors[rand(0...my_doctors.length)].id, 
@@ -77,9 +92,14 @@ end
 end
 
 # 08 - Populate 'my_doctor_diplomas'
+puts "  > Populating 'my_doctor_diplomas'"
 30.times do |x|
-  my_doctor_diplomas << DoctorSpecialty.create(date_obtained: Faker::Date.backward(days: 4000),
+  my_doctor_diplomas << DoctorDiploma.create(date_obtained: Faker::Date.backward(days: 4000),
                                                expiration_date: Faker::Date.forward(days: 4000),
                                                doctor_id: my_doctors[rand(0...my_doctors.length)].id, 
-                                               specialty_id: my_specialties[rand(0...my_specialties.length)].id)
+                                               diploma_id: my_diplomas[rand(0...my_diplomas.length)].id)
 end
+
+puts "[POPULATE SECTION - END]"
+
+puts "*** Ended at #{Time.now} ***"
